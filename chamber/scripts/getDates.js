7b4b096fb46f87e8e8e1e42a104a4a9c2e04228d
CaptainCarnivore <ausjensen0451@byui.edu>
2023-09-30 16:07:37 -0700

    added discover chamber page

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -3,3 +3,28 @@ const todayDate = document.querySelector('#lastModified');
 let lastModif = new Date(document.lastModified);
 
 todayDate.innerHTML = "Last modified: " + lastModif;
+
+const msToDays = 84600000;
+let today = new Date();
+let todayMS = Date.now();
+let lastVisit = Number(window.localStorage.getItem("lastVisit-ls")) || todayMS;
+
+const lastVisited = document.querySelector('#lastVisited');
+
+if (todayMS == lastVisit)
+{
+    lastVisited.textContent = "Welcome! Let us know if you have any questions.";
+} else if ((todayMS - lastVisit) <= 84600000) {
+    lastVisited.textContent = "Back so soon! Awesome!";
+} else if ((todayMS - lastVisit) > 84600000) {
+    lastVisited.textContent = "You last visited " + ((todayMS - lastVisit) / msToDays).toFixed(0) + "days ago";
+}
+
+localStorage.setItem("lastVisit-ls", todayMS);
+
+//var date1 = new Date();
+//window.localStorage.setItem("date", date1.getTime());
+
+//var date2 = new Date(Number(window.localStorage.getItem("date")));
+//window.localStorage.setItem("date", date2.getTime());
+//alert(date2);
