20d0ba0244c02be7a1b1da7b55da37fe79ee5f4d
CaptainCarnivore <ausjensen0451@byui.edu>
2023-10-23 14:57:05 -0700

    final touches (i hope)

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -1,18 +1,15 @@
 const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
 const body = document.querySelector("body");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🌙")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
+
 		modeButton.textContent = "☀️";
 		body.style.background = "#000";
 		console.log(cardLink);
 	} else {
-		main.style.background = "#368f8bff";
-		main.style.color = "#f4edeaff";
-		body.style.background = "#368f8bff";
+
+		body.style.background = "none";
 		modeButton.textContent = "🌙";
 	}
 });
